b6f07e30b9b11a3d97c6144303a9af3a0ed21ebb
Stuart Aird <stuartaird@pm.me>
2021-09-23 11:26:00 +0100

    heroku config added

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const port = 9090;
+const { PORT = 9090 } = process.env;
 
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}...`);
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}...`);
 });
